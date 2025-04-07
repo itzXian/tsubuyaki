0395d06cd572f0b1457671034f3a5121f2141e44
itzXian <xixian.2106@gmail.com>
2025-04-07 15:48:03 +0800

    change avatar

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'itzXian',
   bio: "Don't ask to ask, just ask.",
   links: [
